ec011149ba77b8a65b0f97355016e953d02bc6e3
a-clap <clapinskiadam@gmail.com>
2022-12-09 20:23:46 +0100

    Add Entry widget for keyboards.

---
+++ pkg/keyboard/numeric/entry.go
@@ -0,0 +1,37 @@
+package numeric
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/widget"
+)
+
+type Entry struct {
+	*widget.Entry
+}
+
+func NewEntry(placeholder string, w *widget.Entry) *Entry {
+	e := Entry{
+		Entry: w,
+	}
+	e.SetPlaceHolder(placeholder)
+	e.ExtendBaseWidget(e)
+
+	return &e
+}
+
+func (e *Entry) Tapped(*fyne.PointEvent) {
+	if e.Disabled() {
+		return
+	}
+
+	w, _ := Show(e)
+	w.Show()
+}
+
+func (e *Entry) Get() string {
+	return e.Text
+}
+
+func (e *Entry) Set(value string) {
+	e.SetText(value)
+}
