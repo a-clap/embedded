68e34fc3551794548ff6e0d3471292aa034936e0
a-clap <clapinskiadam@gmail.com>
2023-02-05 07:42:01 +0100

    Distillation pkg - just outline.

--- internal/distillation/interfaces.go
+++ internal/distillation/interfaces.go
@@ -3,4 +3,12 @@
  * Use of this source code is governed by a MIT-style license that can be found in the LICENSE file.
  */
 
-package dest
+package distillation
+
+type TemperatureSensor interface {
+	Temperature() (float32, error)
+}
+
+type Heater interface {
+	Set(power uint) error
+}
--- internal/distillation/phase.go
+++ internal/distillation/phase.go
@@ -3,7 +3,7 @@
  * Use of this source code is governed by a MIT-style license that can be found in the LICENSE file.
  */
 
-package dest
+package distillation
 
-type State struct {
+type Phase struct {
 }
