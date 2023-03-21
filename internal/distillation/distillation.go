15f3c6c1056cbb026e3deaa015a11ea8862abe74
a-clap <clapinskiadam@gmail.com>
2023-03-21 18:22:51 +0100

    Automatic temperatures update.

--- internal/distillation/distillation.go
+++ internal/distillation/distillation.go
@@ -6,6 +6,7 @@
 package distillation
 
 import (
+	"sync"
 	"sync/atomic"
 	"time"
 
@@ -20,13 +21,19 @@ type Handler struct {
 	PTHandler      *PTHandler
 	GPIOHandler    *GPIOHandler
 	running        atomic.Bool
+	updateInterval time.Duration
+	finish         chan struct{}
+	finished       chan struct{}
 	Process        *process.Process
 }
 
 func New(opts ...Option) (*Handler, error) {
 	h := &Handler{
-		Engine:  gin.Default(),
-		running: atomic.Bool{},
+		Engine:         gin.Default(),
+		running:        atomic.Bool{},
+		finish:         make(chan struct{}),
+		finished:       make(chan struct{}),
+		updateInterval: 1 * time.Second,
 	}
 
 	// Options
@@ -47,21 +54,53 @@ func New(opts ...Option) (*Handler, error) {
 
 func (h *Handler) Run(addr ...string) error {
 	h.running.Store(true)
-	if h.PTHandler != nil {
-		go h.updatePTs()
-	}
+	go h.updateTemperatures()
+
 	err := h.Engine.Run(addr...)
 	h.running.Store(false)
+	close(h.finish)
+	for range h.finished {
+	}
 
 	return err
 }
 
-func (h *Handler) updatePTs() {
-	for h.running.Load() {
-		<-time.After(1 * time.Second)
-		errs := h.PTHandler.Update()
-		if errs != nil {
-			log.Debug(errs)
-		}
+func (h *Handler) updateTemperatures() {
+	var wg sync.WaitGroup
+	if h.PTHandler != nil {
+		wg.Add(1)
+		go func() {
+			for h.running.Load() {
+				select {
+				case <-h.finish:
+					break
+				case <-time.After(h.updateInterval):
+					errs := h.PTHandler.Update()
+					if errs != nil {
+						log.Debug(errs)
+					}
+				}
+			}
+			wg.Done()
+		}()
 	}
+	if h.DSHandler != nil {
+		wg.Add(1)
+		go func() {
+			for h.running.Load() {
+				select {
+				case <-h.finish:
+					break
+				case <-time.After(h.updateInterval):
+					errs := h.DSHandler.Update()
+					if errs != nil {
+						log.Debug(errs)
+					}
+				}
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	close(h.finish)
 }
